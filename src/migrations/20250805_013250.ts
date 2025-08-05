cf7a1969c2d6161bf635bf733c2ea2be31c37c4e
arif-wright <arif.wright@gmail.com>
2025-08-04 20:33:12 -0500

    Register Homepage collection in payload.config.ts and add Homepage schema v20

---
+++ src/migrations/20250805_013250.ts
@@ -0,0 +1,11 @@
+import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+
+export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "hero_section" ADD COLUMN "description" varchar;`)
+}
+
+export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
+  await db.execute(sql`
+   ALTER TABLE "hero_section" DROP COLUMN "description";`)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250805_003447 from './20250805_003447';
+import * as migration_20250805_013250 from './20250805_013250';
 
 export const migrations = [
   {
     up: migration_20250805_003447.up,
     down: migration_20250805_003447.down,
-    name: '20250805_003447'
+    name: '20250805_003447',
+  },
+  {
+    up: migration_20250805_013250.up,
+    down: migration_20250805_013250.down,
+    name: '20250805_013250'
   },
 ];
