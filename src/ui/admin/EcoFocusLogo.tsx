3754f0f60403aac6c6c871a29fd27187a2ad9a89
arif-wright <arif.wright@gmail.com>
2025-09-16 14:41:34 -0500

    Adding Graph Functionality

--- src/ui/admin/EcoFocusLogo.tsx
+++ src/ui/admin/EcoFocusLogo.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
+import Image from 'next/image'
 
 const EcoFocusLogo: React.FC = () => {
   return (
     <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-      <img src="/ecofocus-logo.svg" alt="EcoFocus" width={120} height={28} style={{ height: 28 }} />
+      <Image src="/ecofocus-logo.svg" alt="EcoFocus" width={120} height={28} priority />
     </div>
   )
 }
